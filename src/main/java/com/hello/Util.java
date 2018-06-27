eab3624b93dcf30b7fa9f4bd131fb2dccf02d2ff
aselaindika <wejerathne@gmail.com>
2018-06-27 18:32:49 +0530

    package com.hello;
    public class Util {
    
    	public static void printHelloWorld() {
        	//print Hello World in console
            System.out.println( "Hello World! How Are You?" );
    
    	}
    }

--- src/main/java/com/hello/Util.java
+++ src/main/java/com/hello/Util.java
@@ -4,6 +4,7 @@ public class Util {
 
 	public static void printHelloWorld() {
     	//print Hello World in console
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! How Are You?" );
+        
 	}
 }
